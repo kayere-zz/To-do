428220ce17a9c9d1583659c34d0607b67e632ee3
Peter Kayere <brainhesley3@gmail.com>
2020-12-07 18:25:13 +0300

    - Edited Adapter

--- app/src/main/java/com/example/to_dolist/main_activity/ListAdapter.kt
+++ app/src/main/java/com/example/to_dolist/main_activity/ListAdapter.kt
@@ -5,20 +5,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.to_dolist.R
+import com.example.to_dolist.db.ToDo
 import com.google.android.material.textview.MaterialTextView
 
-class ListAdapter(private val list: List<String>) : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+class ListAdapter(private val list: List<ToDo>) : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
             ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent))
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        // TODO: 07/12/2020  add implementation 
+        holder.textView.text = list[position].content
     }
 
     override fun getItemCount(): Int = list.size
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val textView = itemView.findViewById<MaterialTextView>(R.id.textView)
+        val textView: MaterialTextView = itemView.findViewById(R.id.textView)
     }
 }
