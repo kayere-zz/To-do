e54a4fe54c254e6e4cd47be29afe02e44a1b54ea
Peter Kayere <brainhesley3@gmail.com>
2020-12-07 18:50:31 +0300

    - Edited start up

--- app/src/main/java/com/example/to_dolist/main_activity/MainActivity.kt
+++ app/src/main/java/com/example/to_dolist/main_activity/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.to_dolist.main_activity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.example.to_dolist.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,5 +28,18 @@ class MainActivity : AppCompatActivity() {
                 show()
             }
         }
+
+        val helper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT){
+            override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        helper.attachToRecyclerView(todoList)
     }
 }
